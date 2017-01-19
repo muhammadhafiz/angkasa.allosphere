f9c8d25353489257f9bf9d17d7fd3ddc5692e71b
hafiz <muhammadhafiz.wanrosli@gmail.com>
2017-01-19 15:55:40 -0800

    merged alloproject submodule branch

---
+++ initproject.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#git init
+#git submodule init
+#git submodule update
+
+git submodule update --init GLV
+git submodule update --init --depth 100
+
+# git submodule add --depth 1 https://github.com/AlloSphere-Research-Group/AlloSystem.git AlloSystem
+# git submodule add --depth 1 https://github.com/AlloSphere-Research-Group/GLV.git GLV
+# git submodule add --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
+# git submodule add --depth 1 https://github.com/rbtsx/cuttlebone.git cuttlebone
+
+# git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/AlloSystem.git AlloSystem
+# git clone -b cmake --depth 1 https://github.com/AlloSphere-Research-Group/GLV.git GLV
+# git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
+# git clone --depth 1 https://github.com/rbtsx/cuttlebone.git cuttlebone
+#git submodule add https://github.com/AlloSphere-Research-Group/vsr.git AlloSystem
+
+#git add -A
+#git commit -m "Initial commit for project"
+
+# Remove remote of AlloProject to avoid committing to it
+# git remote set-url --push origin no_push
+
+
---
+++ install_all_dependencies.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# AlloProject dependencies install script
+
+if [ ! -d "AlloSystem/allocore" ]; then
+    echo "Please run initproject.sh script before this one."
+    exit 1
+fi
+
+AlloSystem/install_all_dependencies.sh
+
+ROOT=`pwd`
+PLATFORM=`uname`
+ARCH=`uname -m`
+echo Installing for $PLATFORM $ARCH from $ROOT 
+
+if [ `which apt-get` ]; then
+	echo "Found apt-get"
+	sudo apt-get cmake
+	
+elif [ `which port` ]; then
+	echo "Found MacPorts"
+	sudo port install cmake
+
+elif [ `which brew` ]; then
+	echo "Found Homebrew"
+	brew update
+	brew install cmake
+
+elif [ `uname | grep MINGW` ]; then
+	echo "Found MinGW / MSYS"
+	echo "Please install CMAKE manually"
+
+else
+	echo "Error: No suitable package manager found."
+	echo "Error: Install apt-get, MacPorts, or Homebrew and try again."
+fi
+
